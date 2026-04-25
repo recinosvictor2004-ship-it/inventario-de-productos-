cfd2d89a47a5b1b1be3dbb12b4274e9373edb46d
recinosvictor2004 <recinosvictor2004@gmail.com>
2026-04-24 20:56:56 -0600

    feat: :sparkles: se crea agregar producto

---
+++ inventario.py
@@ -0,0 +1,8 @@
+inventario = []
+
+def agregar_producto(nombre, precio, cantidad):
+    inventario.append({
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad
+    })
